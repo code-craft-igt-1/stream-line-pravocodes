7fbd6812fa586822958f250c2840c5655d19bcd2
Praveen Kumar <Praveen.Kumar_4@philips.com>
2024-09-26 13:57:21 +0530

    addded Gtest

--- main.cpp
+++ main.cpp
@@ -1,43 +1,4 @@
-#include <iostream>
-#include <random>
-#include <iomanip> 
-#include <sstream> 
-
-void initializeRandom() {
-    std::srand(static_cast<unsigned int>(std::time(0)));
-}
-
-double generateTemperature() {
-    return 95.0 + static_cast<double>(rand()) / (static_cast<double>(RAND_MAX / (105.0 - 95.0)));
-}
-
-int generatePulseRate() {
-    return rand() % (100 - 60 + 1) + 60;
-}
-
-int generateSpO2() {
-    return rand() % (100 - 90 + 1) + 90; 
-}
-
-std::string generateSensorData() {
-    initializeRandom();
-    double temperature = generateTemperature();
-    int pulseRate = generatePulseRate() ;
-    int spo2 = generateSpO2();
-
-    std::ostringstream dataStream;
-    dataStream << std::fixed << std::setprecision(1) << temperature << "," 
-               << pulseRate << "," << spo2;
-    return dataStream.str();
-}
-
-void sendSensorData() {
-    const int readingsCount = 50; 
-    for (int i = 0; i < readingsCount; ++i) {
-        std::string sensorData = generateSensorData();
-        std::cout << sensorData << std::endl; 
-    }
-}
+#include "./dataGenerator.h"
 
 int main() {
     sendSensorData(); 
